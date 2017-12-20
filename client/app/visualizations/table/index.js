74d761d771c9ae3e19342e8b380e3500a8509701
onelittlewolfe <onelittlewolfe@gmail.com>
2017-12-20 17:02:17 +0000

    formula update

--- client/app/visualizations/table/index.js
+++ client/app/visualizations/table/index.js
@@ -108,7 +108,7 @@ function formatExtraCols(col, $filter, clientConfig, totals, rows) {
     'SAC (€/Sub)': (numberFormat(totals['Total Spend (€)'])) && (totals['Total Subscriptions']) ? numberFormat(totals['Total Spend (€)'] / totals['Total Subscriptions'], 2) : numberFormat(totals['SAC (€/Sub)'] * 100, 2),
     'CTR( %)': totals['Total Clicks'] && totals['Total Impressions'] ? numberFormat(totals['Total Clicks'] / totals['Total Impressions'] * 100, 2) : numberFormat(totals['CTR( %)'] * 100, 2),
     'CVR(%)': totals['Total Subscriptions'] && totals['Total Clicks'] || totals['Subscriptions'] && totals['Sessions'] ? numberFormat(totals['Total Subscriptions'] / totals['Total Clicks'] * 100, 2) || numberFormat(totals['Subscriptions'] / totals['Sessions'] * 100, 2) : numberFormat(totals['CVR%'] * 100, 2),
-    'CPC(€)': totals['Total Spend (€)'] && totals['Total Clicks'] ? numberFormat(totals['Total Spend (€)'] / totals['Total Clicks'] * 100, 2) : numberFormat(totals['CPC(€)'], 2),
+    'CPC(€)': totals['Total Spend (€)'] && totals['Total Clicks'] ? numberFormat(totals['Total Spend (€)'] / totals['Total Clicks'], 2) : numberFormat(totals['CPC(€)'], 2),
     'CPM (€)': totals['Total Spend (€)'] && totals['Total Impressions'] ? numberFormat((totals['Total Spend (€)']) / (totals['Total Impressions']) * 1000, 2) : numberFormat(totals['CPM (€)'], 2),
     'Avg Position': numberFormat(totals['Avg Position'] / numberFormat(rows.length), 2)
   };
